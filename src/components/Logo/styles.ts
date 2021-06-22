82f0175f831ebaba4ccad5dfdc258c7fb203159b
Erick Bernardo <erickvictor@gmail.com>
2021-06-22 13:28:20 -0400

    Add option to size on logo

--- src/components/Logo/styles.ts
+++ src/components/Logo/styles.ts
@@ -1,3 +1,21 @@
-import styled from 'styled-components'
+import styled, { css } from 'styled-components'
 
-export const Wrapper = styled.div``
+import { LogoProps } from '.'
+
+const wrapperModifiers = {
+  normal: () => css`
+    width: 14.5rem;
+    height: 3.15rem;
+  `,
+
+  large: () => css`
+    width: 18rem;
+    height: 3.9rem;
+  `
+}
+
+export const Wrapper = styled.div<LogoProps>`
+  ${({ size }) => css`
+    ${!!size && wrapperModifiers[size]}
+  `}
+`
